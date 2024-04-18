f7bbd2bda88dbdd4654031cb725cce6cb26d3526
pkum6228 <pkum6228@sysco.com>
2024-04-18 14:54:04 +0530

    Strings

---
+++ Inter/Strings.py
@@ -0,0 +1,44 @@
+# String are Immutable, ordred
+
+str1 = "i'm Prashanth Kumar"        #single quotes in double quotes
+print(str1)
+
+if "P" in str1:             #checks whether character is present
+    print("Yes!!!")
+
+for i in str1:
+    print(i)
+
+
+str2 = "    God Dammn   "       #strips unwanted spaces
+print(str2)
+str2 = str2.strip()
+print(str2)
+
+str1 = str1.lower()
+print(str1)
+print(str1.upper())         #lower and upper cases
+
+print(str1.startswith("i'm"))
+print(str1.endswith("kumar"))           #startswith and endswith funtions
+
+print(str1.find("p"))
+print(str1.count("a"))              #find and count function
+
+print(str1.replace("i'm","Hello"))          #finds and replace char and strings
+
+li = str1.split(" ")
+print(li)                   #string to list and list to string
+print("".join(li))
+print("-----".join(li))
+
+input1 = 1000101
+list1 = input1.__str__()
+print(list1[2])
+
+a= "prashanth"
+b = 98.9999999
+print("Hi i'm %s" % a)
+print("Hi i'm {} and i scored {:.2f}".format(a,b))             #Formating
+print(f"Hi i'm {a} and i scored {b}")
+
